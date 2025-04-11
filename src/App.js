e8d5ef2ff2b6bd28af1a3bccda62ee82bab08466
Abuh <ak.abrahamkibet@gmail.com>
2025-04-11 12:26:10 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+// const App = () => {
+//   const [input, setInput] = useState("");
+//   function Output(e) {
+//     setInput(e.target.value);
+//   }
+//   return (
+//     <>
+//       <h1> {input}</h1>
+//       <input type="text" onInput={Output} />
+//     </>
+//   );
+// };
+
+// export default App;
+
+// const App = () => {
+//   const [fname, setFName] = useState("");
+//   const [lname, setLName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [h1Text, setH1Text] = useState("");
+
+//   function handleFname(e) {
+//     setFName(e.target.value);
+//   }
+//   function handleLname(e) {
+//     setLName(e.target.value);
+//   }
+//   function handleEmail(e) {
+//     setEmail(e.target.value);
+//   }
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     setH1Text(`First Name:$(fname) Last Name:${lname} Email: ${email}`);
+//     setFName("");
+//     setLName("");
+//     setEmail("");
+//   }
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input type="text" type="fname" onInput={handleFname} value={fname} />
+//         <input type="text" type="lname" onInput={handleLname} value={lname} />
+//         <input type="text" type="email" onInput={handleEmail} value={email} />
+//         <button type='submit'>Submit</button>
+//       </form>
+//       <h1>{h1Text}</h1>
+//     </>
+//   );
+// };
+
+// export default App;
+
+const App = () => {
+  const [fname, setFName] = useState("");
+  const [lname, setLName] = useState("");
+  const [email, setEmail] = useState("");
+  const [output, setOutput] = useState("");
+
+  function handleFname(e) {
+    setFName(e.target.value);
+  }
+  function handleLname(e) {
+    setLName(e.target.value);
+  }
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setOutput(`${fname} ${lname} ${email}`);
+    setFName("");
+    setLName("");
+    setEmail("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fname"
+          value={fname}
+          placeholder="First Name"
+          onInput={handleFname}
+        />
+        <input
+          type="text"
+          name="lname"
+          value={lname}
+          placeholder="Last Name"
+          onInput={handleLname}
+        />
+        <input
+          type="text"
+          name="email"
+          value={email}
+          placeholder="Email"
+          onInput={handleEmail}
+        />
+        <button>Submit</button>
+      </form>
+      <h1>{output}</h1>
+    </>
   );
-}
+};
 
 export default App;
